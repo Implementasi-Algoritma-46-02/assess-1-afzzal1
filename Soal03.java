dfe209ed448ea751e826b154ffb8b6c28dd0652d
Afzzal1 <afzzalfaisal23@gmail.com>
2024-10-29 16:46:00 +0700

    assesment

--- Soal03.java
+++ Soal03.java
@@ -1,6 +1,30 @@
-public class Soal03 {
+import java.util.Scanner;
 
-	public static void main(final String[] args) {
-		// Kerjakan soalnya di sini
-	}
+public class soal03 {
+
+    public static String waktu(String jam, int waktu) {
+        String Waktu;
+        
+        if (waktu >= 1 && waktu <= 24) {
+            Waktu = "jam";
+        } else if (waktu >= 0 && waktu <= 3) {
+            Waktu = "menit";
+        } else if (waktu >=  && waktu <= )
+            waktu = "tambah menit";
+         else {
+            
+    }
+        
+        return " jam" + "menit";
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String jam = scanner.nextLine();   
+        String menit = scanner.nextLine();
+        int waktu = scanner.nextInt();  
+ 
+        System.out.println(waktu(jam, waktu));
+    }
 }
--- Soal04.java
+++ Soal04.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Soal04 {
 
 	public static void main(final String[] args) {
-		// Kerjakan soalnya di sini
+		
 	}
 }
